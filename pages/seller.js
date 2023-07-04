0e646f0abae413f90a818704f58607304ad01479
박정현 <pjh9996@naver.com>
2023-07-04 14:04:59 +0900

    feat: 판매자 페이지 기능 테스트(등록/조회)

---
+++ pages/seller.js
@@ -0,0 +1,110 @@
+import React, { useState, useRef } from 'react';
+
+import Button from '@mui/material/Button';
+
+import Grid from '@mui/material/Grid';
+
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+import ImageListItemBar from '@mui/material/ImageListItemBar';
+
+import TextField from '@mui/material/TextField';
+
+import Dialog from '@mui/material/Dialog';
+import DialogActions from '@mui/material/DialogActions';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogTitle from '@mui/material/DialogTitle';
+
+import Input from '@mui/material/Input';
+
+const uitest = () => {
+  const [registerDialogOpen, setRegisterDialogOpen] = useState(false);
+
+  const [imageFile, setImageFile] = useState(null);
+  const [pdfFile, setPdfFile] = useState(null);
+
+  const [fileList, setFileList] = useState([]);
+
+  return (
+    <>
+      <Button variant="contained" onClick={() => setRegisterDialogOpen(true)}>등록</Button>
+      <Button 
+        sx={{marginLeft: 1}}
+        variant="outlined"
+        onClick={() => {
+          fetch('http://localhost:8080/list')
+            .then((res) => res.json())
+            .then((data) => setFileList(data))
+            .catch((error) => console.log(error))
+        }}
+      >
+        조회
+      </Button>
+      <ImageList cols={5} gap={8}>
+        {fileList.map((file, index) => (
+          <ImageListItem key={index}>
+            <img
+              style={{
+                cursor: 'pointer',
+              }}
+              src={`http://localhost:8080/files/${file.image}`}
+              alt={file.image}
+              loading="lazy"
+            />
+            <ImageListItemBar
+              title={file.pdf}
+              position="below"
+            />
+          </ImageListItem>
+        ))}
+      </ImageList>
+
+      <Dialog open={registerDialogOpen} onClose={() => setRegisterDialogOpen(false)}>
+        <DialogTitle>등록</DialogTitle>
+        <DialogContent>
+          <Input
+            type='file'
+            onChange={(event) => {
+              setImageFile(event.target.files[0]);
+            }}
+          />
+          <Input
+            type='file'
+            onChange={(event) => {
+              setPdfFile(event.target.files[0]);
+            }}
+          />
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={() => setRegisterDialogOpen(false)}>닫기</Button>
+          <Button
+            onClick={() => {
+              setRegisterDialogOpen(false);
+
+              const formData = new FormData();
+              formData.append('image', imageFile);
+              formData.append('pdf', pdfFile);
+
+              fetch('http://localhost:8080/register', {
+                method: 'POST',
+                body: formData,
+              })
+                .then(response => response.json())
+                .then(data => {
+                  console.log(data);
+                })
+                .catch(error => {
+                  console.error(error);
+                });
+            }}
+          >
+            등록
+          </Button>
+        </DialogActions>
+      </Dialog>
+    </>
+  )
+}
+
+export default uitest;
